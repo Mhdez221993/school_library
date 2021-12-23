26bdcb6e7c1b6f456c7743186a90726326537834
Moises <mhdezcoronado@gmail.com>
2021-12-23 23:19:33 +0100

    Update student parameters

--- student.rb
+++ student.rb
@@ -1,8 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
+    @classroom = classroom
   end
 
   def play_hooky
@@ -10,6 +13,7 @@ class Student < Person
   end
 
   def classroom=(classroom)
+    @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
