c82ee238f34acd6b24e71764ad88baaa2868ec0d
Moises <mhdezcoronado@gmail.com>
2021-12-23 17:42:09 +0100

    Remove unnessessary code

--- student.rb
+++ student.rb
@@ -1,12 +1,8 @@
 require_relative 'person'
-require_relative 'classroom'
 
 class Student < Person
-  attr_reader :classroom
-
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
@@ -14,7 +10,6 @@ class Student < Person
   end
 
   def classroom=(classroom)
-    @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
