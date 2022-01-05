9966fc9aebb5b17ca929921cde0d830fdeaf54c3
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-05 09:22:25 +0100

    load people file

--- file_handler.rb
+++ file_handler.rb
@@ -6,4 +6,19 @@ require_relative 'classroom'
 require_relative 'rental'
 
 module HandlersFile
+  def load_peoples
+    file = 'peoples.json'
+    if File.exist? file
+      JSON.parse(File.read(file)).map do |people|
+        if people['instance'] == 'teacher'
+          teacher = Teacher.new(people['specialization'], people['age'], people['name'])
+          teacher.id = people['id']
+          teacher
+        
+      end
+    else
+      []
+    end
+  end
+
 end
