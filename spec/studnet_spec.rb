78580d48e0c56d88b9467f98e444906e9e4aa2f8
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-06 09:44:53 +0100

    add test to teacher

--- spec/studnet_spec.rb
+++ spec/studnet_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../student'
 
 describe Student do
-  student = Student.new(27, 'MhdezCoronado')
+  before(:each) do
+    @student = Student.new(27, 'MhdezCoronado')
+  end
 
   it('Shoud check if student is instance of Student') do
-    expect(student).to be_instance_of(Student)
+    expect(@student).to be_instance_of(Student)
   end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../teacher'
 
 describe Teacher do
-  teacher = Teacher.new(27, 'MhdezCoronado')
+  before(:each) do
+    @teacher = Teacher.new(27, 'MhdezCoronado')
+  end
 
   it('Shoud check if teacher is instance of Teacher') do
-    expect(teacher).to be_instance_of(Teacher)
+    expect(@teacher).to be_instance_of(Teacher)
   end
 end
