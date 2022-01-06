f31b06f3372e5921a4c59e8e7f2acb1d0903f522
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-06 09:57:03 +0100

    add name test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,10 +2,14 @@ require_relative '../teacher'
 
 describe Teacher do
   before(:each) do
-    @teacher = Teacher.new(27, 'MhdezCoronado')
+    @teacher = Teacher.new('teacher', 27, 'MhdezCoronado')
   end
 
   it('Shoud check if teacher is instance of Teacher') do
     expect(@teacher).to be_instance_of(Teacher)
+  end
+
+  it('Shoud check if name is MhdezCoronado') do
+    expect(@teacher.name).to eq('MhdezCoronado')
   end
 end
