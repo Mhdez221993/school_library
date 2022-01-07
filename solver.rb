1506179e11620e55d65ddf5e2ddc61059b032e4a
Moises <mhdezcoronado@gmail.com>
2022-01-07 09:49:51 +0100

    Add TDD for fizzbuzz method

--- solver.rb
+++
@@ -1,3 +0,0 @@
-class Solver
-  def factorial(n)
-end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,11 +1,25 @@
 describe Solver do
   before(:each) do
     @solver = Solver.new
-    
   end
 
- 
-  it('should check the sum of factorial') do
-    expect(@solver.factorial(3)).to eq 18
+  describe '#factorial' do
+    it('should check the sum of factorial') do
+      expect(@solver.factorial(3)).to eq 6
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'Should verify if the number is multiple of 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+
+    it 'Should verify if the number is multiple of 3' do
+      expect(@solver.fizzbuzz(6)).to eq 'fizz'
+    end
+
+    it 'Should verify if the number is multiple of 5' do
+      expect(@solver.fizzbuzz(10)).to eq 'buzz'
+    end
   end
 end
