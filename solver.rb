ceaad2a63b988b64ed54eb1defb0d73a919312fc
Moises <mhdezcoronado@gmail.com>
2022-01-07 10:15:41 +0100

    Implement factorial & fizzbuzz methods

--- solver.rb
+++ solver.rb
@@ -1,13 +1,21 @@
 class Solver
-  def factorial(_num)
-    120
+  def factorial(num)
+    return 1 if num.zero?
+
+    num * factorial(num - 1)
   end
 
   def reverse(str)
     str.reverse
   end
 
-  def fizzbuzz(_n)
-    'fizzbuzz'
+  def fizzbuzz(num)
+    if (num % 3).zero? & (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    end
   end
 end
